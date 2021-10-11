f7417d95328079d9b1861cdc1369620069f349dd
VVatermelon <54984293+VVatermalon@users.noreply.github.com>
2021-10-11 22:04:26 +0300

    Started parser

--- src/main/java/parser/impl/PyramidParserImpl.java
+++ src/main/java/parser/impl/PyramidParserImpl.java
@@ -1,7 +1,22 @@
 package parser.impl;
 
+import entity.Pyramid;
 import parser.PyramidParser;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PyramidParserImpl implements PyramidParser {
-    
+    static final String SPLITTER = "//s, //,";
+    public List<Pyramid> parse(List<String> input) {
+        for (String line: input) {
+            String[] splittedLine = line.trim().split(SPLITTER);
+            if(splittedLine.length != 15) {
+                return new ArrayList<>();
+            }
+            for (String part: splittedLine) {
+
+            }
+        }
+    }
 }
